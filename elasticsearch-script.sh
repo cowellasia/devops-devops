ba2de4e66ff22dc65627dfd9d12145cccee2fe04
herotl2005 <nguyendanghung2008@gmail.com>
2016-07-25 14:03:02 +0700

    Create elasticsearch-script.sh

---
+++ elasticsearch-script.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install openjdk-7-jre
+java -version
+
+echo "deb https://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+sudo apt-get update && sudo apt-get install elasticsearch
